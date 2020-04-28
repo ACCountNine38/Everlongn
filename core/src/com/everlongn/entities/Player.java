8328bf048bc981fca64e1f4deb3c464c659da777
ACCount-Nine38 <acc938live@gmail.com>
2020-04-27 20:10:19 -0400

    Lighting

--- core/src/com/everlongn/entities/Player.java
+++ core/src/com/everlongn/entities/Player.java
@@ -1,7 +1,9 @@
 package com.everlongn.entities;
 
+import box2dLight.PointLight;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 import com.everlongn.assets.Images;
@@ -9,12 +11,15 @@ import com.everlongn.game.ControlCenter;
 import com.everlongn.game.Inputs;
 import com.everlongn.game.TextManager;
 import com.everlongn.tiles.Tile;
+import com.everlongn.world.WorldGenerator;
 
 import java.awt.*;
 
 import static com.everlongn.utils.Constants.PPM;
 
 public class Player extends Creature {
+
+    private PointLight light;
 
     public Player(ControlCenter c, float x, float y, int width, int height) {
         super(c, x, y, width, height);
@@ -25,10 +30,15 @@ public class Player extends Creature {
         speed = 5;
 
         type.add("player");
+        light = new PointLight(WorldGenerator.rayHandler, 100, Color.BLACK, 250/PPM,
+                body.getPosition().x,
+                body.getPosition().y);
     }
 
     @Override
     public void tick() {
+        light.setPosition(body.getPosition().x,
+                body.getPosition().y);
         inputUpdate();
         cameraUpdate();
     }
@@ -42,7 +52,7 @@ public class Player extends Creature {
             horizontalForce = 1;
         }
         if(Gdx.input.isKeyJustPressed(Input.Keys.W)) {
-            body.applyForceToCenter(0, 1250, false);
+            body.applyForceToCenter(0, 800, false);
         }
         if(Gdx.input.isKeyPressed(Input.Keys.P)) {
             System.exit(1);
@@ -62,8 +72,9 @@ public class Player extends Creature {
     @Override
     public void render(SpriteBatch batch) {
         TextManager.render(x + " " + y);
+
         batch.draw(Images.temp, body.getPosition().x*PPM - width/2,
-                body.getPosition().y*PPM + height/2, width, height);
+                body.getPosition().y*PPM - height/2, width, height);
 
         batch.setProjectionMatrix(c.getCamera().combined);
     }
